5da3b3fa3e3bd7567fb8c6d0d5fc3e942121f20b
Henrik Wallerman <wallermanhenrik@gmail.com>
2022-04-12 20:12:21 +0200

    Buy orders can be created.

--- src/main/java/com/example/backendprojectone/controllers/ItemController.java
+++ src/main/java/com/example/backendprojectone/controllers/ItemController.java
@@ -1,6 +1,9 @@
 package com.example.backendprojectone.controllers;
 
+import com.example.backendprojectone.models.BuyOrder;
 import com.example.backendprojectone.models.Item;
+import com.example.backendprojectone.repositories.BuyOrderRepository;
+import com.example.backendprojectone.repositories.CustomerRepository;
 import com.example.backendprojectone.repositories.ItemRepository;
 import com.example.backendprojectone.response.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,12 @@ import org.springframework.web.bind.annotation.*;
 public class ItemController {
     @Autowired
     private ItemRepository itemRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private BuyOrderRepository buyOrderRepository;
 
     @RequestMapping()
     public Iterable<Item> getAllItems() {
@@ -26,6 +35,17 @@ public class ItemController {
     public Response addItem(@RequestBody Item i) {
         Response res = new Response("Item added", Boolean.FALSE);
         itemRepository.save(i);
+        res.setStatus(Boolean.TRUE);
+        return res;
+    }
+
+    @PostMapping("/buy")
+    public Response addBuyOrder(@RequestParam long customer, @RequestParam long item) {
+        BuyOrder bo = new BuyOrder();
+        Response res = new Response("Buy order added", Boolean.FALSE);
+        bo.setCustomer(customerRepository.findById(customer).get());
+        bo.setItem(itemRepository.findById(item).get());
+        buyOrderRepository.save(bo);
         res.setStatus(Boolean.TRUE);
         return res;
     }
